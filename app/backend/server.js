67e955fca0d2b910cde275675562dd1418d520b8
AliYasser2003 <aliyasserali0111@gmail.com>
2026-05-16 01:06:33 +0300

    Trigger CI pipelines

--- app/backend/server.js
+++ app/backend/server.js
@@ -5,7 +5,7 @@ const app = express();
 const PORT = 3500;
 
 app.get("/", (req, res) => {
-  res.send("Project 10 Backend Running");
+  res.send("Project 10 Backend Running");  // CI Trigger
 });
 
 app.get("/api", (req, res) => {
